c4b2580a89f044b20f240ed46cd5e0114bb4becc
IronKnife <georgem3132006@gmail.com>
2023-01-11 09:22:41 -0700

    Commented main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,34 +1,34 @@
-
-
 package main;
  
-
-
 import javax.swing.JFrame;
 
 public class Main {
 	public static JFrame window;
     public static void main(String[] args){
-       // Player canvas = new Player(); 
+       // Player canvas = new Player();
+
+       //Configures JFrame window
         window = new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
         window.setTitle("2D Adventure");
         
-       
+       //Adds GamePanel to window
         GamePanel gamePanel = new GamePanel();
         window.add(gamePanel);
         
+        //In fullscreen mode, hide title bar
         gamePanel.config.loadConfig();
         if(gamePanel.fullScreenOn) {
         	window.setUndecorated(true);
         }
+
+        //Sizes and centers window
         window.pack();
-        
         window.setLocationRelativeTo(null);
-        
         window.setVisible(true);
-                
+        
+        //Sets up and runs game
         gamePanel.setUpGame();
         gamePanel.startGameThread();
     } 
